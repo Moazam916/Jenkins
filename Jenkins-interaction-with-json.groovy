2af5a8f801b53905bbf6ba2f5e78fe44d2e5d4c4
moazam <moazamshahzad222@gmail.com>
2024-09-25 20:13:40 -0400

    jenkinsfile to have pretty print enabled

--- Jenkins-interaction-with-json.groovy
+++ Jenkins-interaction-with-json.groovy
@@ -10,11 +10,17 @@ pipeline {
 
                     // Parsing JSON content
                     def jsonSlurper = new groovy.json.JsonSlurper()
+                    // jsonSlurper helps to convert data into map using this class
                     def jsonData = jsonSlurper.parseText(jsonContent)
+                    echo "printing the type of json data: ${jsonData.getClass()}"
                     echo "printing the input json file: ${jsonData}"
                     // Accessing JSON properties (assuming it's a key-value JSON)
-                    echo "Value of key 'name': ${jsonData.name}"
-                    echo "Value of key 'version': ${jsonData.version}"
+                    // echo "Value of key 'name': ${jsonData.name}"
+                    // echo "Value of key 'version': ${jsonData.version}"
+                    //Converting Data back to json from map
+                    def maptojson = groovy.json.JsonOutput.toJson(jsonData)
+                    // enabling pretty print on converted json
+                    def prettyjson = groovy.json.JsonOutput.prettyPrint(maptojson)
                 }
             }
         }
