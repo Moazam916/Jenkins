2af78599de5a793bb468ecf76dfc931a056a4c00
moazam <moazamshahzad222@gmail.com>
2024-09-25 20:34:51 -0400

    jenkinsfile to have pretty print enabled

--- Jenkins-interaction-with-json.groovy
+++ Jenkins-interaction-with-json.groovy
@@ -6,14 +6,26 @@ pipeline {
             steps {
                 script {
                     // Reading the JSON file
+                    def my_map = [:] // intializing the new map
+                    def userInput = input(
+                        id: 'userInput', message: 'Please provide your inputs', parameters: [
+                            string(defaultValue: '', description: 'Enter a name', name: 'name'),
+                            string(defaultValue: '', description: 'Enter a version', name: 'version')
+                        ]
+                    ) // end of user's input
+                    // adding the recieve user input into map
+                    // syntax nameofthemap['<name of key we want to insert>'] = <value>
+                    my_map['App_Name'] = userInput['name']
+                    my_map['App_version'] = userInput['version']
+                    echo "printing the map: ${my_map}"
                     def jsonContent = readFile('sample_data/sample-poc.json')
-
                     // Parsing JSON content
                     def jsonSlurper = new groovy.json.JsonSlurper()
                     // jsonSlurper helps to convert data into map using this class
                     def jsonData = jsonSlurper.parseText(jsonContent)
                     echo "printing the type of json data: ${jsonData.getClass()}"
                     echo "printing the input json file: ${jsonData}"
+                    jsonData.APP_info = my_map
                     // Accessing JSON properties (assuming it's a key-value JSON)
                     // echo "Value of key 'name': ${jsonData.name}"
                     // echo "Value of key 'version': ${jsonData.version}"
